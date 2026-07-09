5568a7972ac4c4837a3cc68bbf0dd17684a100b2
JijeshMon <jijeshjmj@gmail.com>
2026-07-09 21:59:34 +0530

    map added

--- src/pages/About.js
+++ src/pages/About.js
@@ -97,7 +97,7 @@ function About() {
           {/* Embedded Google Map */}
           <iframe
             title="Zenvo Furniture Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5!2d76.4648132!3d10.1372922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0809e2f4b5e8f1%3A0x0!2zMTDCsDA4JzE0LjMiTiA3NsKwMjgnMDIuNiJF!5e0!3m2!1sen!2sin!4v1234567890!5m2!1sen!2sin"
+            src="https://maps.app.goo.gl/xoXhx1Hx257Z9XEe9?g_st=awb"
             width="100%"
             height="100%"
             style={{ border: 0 }}
